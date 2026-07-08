3708a4ee6a8e35eee581e700047517caf5852527
naxfumi <nazrilsmpn06@gmail.com>
2026-07-08 07:14:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1060,7 +1060,10 @@ function txHTML(tx, showDel = false) {
       <div class="tx-info">
         <div class="tx-desc">${tx.desc}</div>
         <div class="tx-meta">${isTransfer ? metaText : metaText}</div>
-      </div>`;
+      </div>
+      <div class="tx-amount ${cls}">${sign} ${fmtFull(tx.amount)}</div>
+      ${showDel ? `<div class="tx-actions"><button class="tx-btn" onclick="deleteTx('${tx.id}')"><svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg></button></div>` : ''}
+    </div>`;
 }
 
 async function deleteTx(id) {
